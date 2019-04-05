e65c6a244e37d7ae8142587f6b3c1a6e64a53364
Dmitry Kutsenko <d2emonium@gmail.com>
2019-04-06 01:10:44 +0300

    Config

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
+import config from './config';
 
-const baseUrl = '//127.0.0.1:4000/languages';
-const Axios = axios.create({
-  baseUrl,
-});
+const Axios = axios.create({ baseURL: config.baseURL });
 
 const getGroups = ({ data }) => Promise.resolve(data.groups);
 
 export const fetchFamilies = () => Axios
-  .get(`${baseUrl}/groups`)
+  .get('/groups')
   .then(getGroups);
 
-export const fetchFamily = (groupId) => Axios
-  .get(`${baseUrl}/subgroups/${groupId || ''}`)
+export const fetchFamily = slug => Axios
+  .get(`/subgroups/${slug || ''}`)
   .then(getGroups);
 
-export const fetchSubgroups = (item) => item && Axios
-  .get(`${baseUrl}/group/${item.slug}`)
+export const fetchSubgroups = item => item && Axios
+  .get(`/group/${item.slug}`)
   .then(getGroups);
